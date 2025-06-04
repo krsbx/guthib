28cac23a250f98022d862c3d4a4aa30ad55ecc8c
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-06-04 19:22:15 +0700

    chore: setup generouted

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./css/index.css";
+import { createRoot } from "react-dom/client";
+import { Routes } from "@generouted/react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root")
+
+if (!root) {
+  throw new Error("Root element not found")
+}
+
+createRoot(root).render(<Routes />);
