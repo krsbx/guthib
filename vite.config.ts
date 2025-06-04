80cf9e6587fa6f1be4cc5a72bb2896a8ac13b756
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-06-04 19:19:10 +0700

    chore: setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import generouted from '@generouted/react-router/plugin';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), generouted()],
+  plugins: [react(), tailwindcss(), generouted()],
 });
