0405fa0515817768208a176fca7c3764ee585a2a
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2025-06-05 16:05:25 +0700

    Updates

--- src/components/cards/repo-skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { SkeletonText } from '@chakra-ui/react';
-
-function GithubRepoSkeleton() {
-  return [1, 2, 3].map((index) => (
-    <SkeletonText mt="4" noOfLines={2} key={`repo-skeleton-${index}`} />
-  ));
-}
-
-export default GithubRepoSkeleton;
